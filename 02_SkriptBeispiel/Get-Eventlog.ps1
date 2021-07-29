f4dd5faf613b23333c40453b1f24f288a5bd73c8
Stefan Ober <StefanO@ppedv.de>
2021-07-29 14:11:30 +0200

    Dokumenation / comment based help added

--- 02_SkriptBeispiel/Get-Eventlog.ps1
+++ 02_SkriptBeispiel/Get-Eventlog.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+Kurzbeschreibung: Skript frägt Anmeldebezogene Events abs
+.DESCRIPTION
+Ausführliche Beschreibung. 
+Dieses Skript ermöglicht die Abfrage von Anmelde- und Abmeldevents sowie fehlgeschlagene Anwendungen
+.PARAMETER EventId
+Mit diesem Parameter muss die EventID angegeben werden.
+4624 AnmeldeEvents
+4625 fehlgeschlagene Anmeldungen
+4634 AbmeldeEvents
+.EXAMPLE
+Get-Eventlog.ps1 -EventId 4624
+
+Index Time          EntryType   Source                 InstanceID Message
+----- ----          ---------   ------                 ---------- -------
+39264 Jul 29 13:56  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+39259 Jul 29 13:56  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+39256 Jul 29 13:56  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+39252 Jul 29 13:56  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+39250 Jul 29 13:56  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet....
+
+Bei dieser Verwendung werden die aktuellesten 5 Anmeldevents vom Eventlog abgefragt
+.EXAMPLE
+zweites Beispiel
+.LINK
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1#comment-based-help-keywords
+#>
 [cmdletBinding(PositionalBinding=$false)] 
 param(
 
@@ -23,6 +51,8 @@ Dokumentation zu Advanced Parametern
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-5.1#attributes-of-parameters
 
 #>
+
+Write-Verbose -Message "Das Skript wurde mit folgenden Werten gestartet EventID: $eventID"
 
 Get-eventlog -LogName Security -ComputerName $ComputerName | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
