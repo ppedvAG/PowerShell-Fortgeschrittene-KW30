a24140e55bb082da3a0ed331b68505e60a382dc3
Stefan Ober <StefanO@ppedv.de>
2021-07-29 13:40:31 +0200

    Advanced Parameter, Pflicht Parameter hinzugefügt

--- 02_SkriptBeispiel/Get-Eventlog.ps1
+++ 02_SkriptBeispiel/Get-Eventlog.ps1
@@ -1,7 +1,12 @@
+[cmdletBinding()]
 param(
-$ComputerName,
-$EventId,
-$Newest
+
+[string]$ComputerName = "localhost",
+
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[int]$Newest = 5
 )
 
 Get-eventlog -LogName Security -ComputerName $ComputerName | Where-Object EventId -eq $EventId | Select-Object -First $Newest
