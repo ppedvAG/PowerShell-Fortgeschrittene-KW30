9422e77e6b3452329e683773bf839cfd786d2407
Stefan Ober <StefanO@ppedv.de>
2021-07-30 14:47:39 +0200

    Beispiel für Klassen und Enums

---
+++ 08_Klassen-Enum/example.ps1
@@ -0,0 +1,22 @@
+﻿class Auto
+{
+     [int]$Türen
+     static [int]$numberOfWheels = 4
+     [string]$Marke
+
+     [AutoFarben]$Farbe
+}
+
+Enum AutoFarben
+{
+    Blau
+    Schwarz
+    Grün
+    Rot
+}
+
+$car = New-Object Auto
+$car.Türen = 2
+$car.Marke = "Lada"
+$car.Farbe = [AutoFarben]::Grün
+
